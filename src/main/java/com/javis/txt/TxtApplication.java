1a34a5f8237c11c11e309888c07681252889346f
GD-Yoon <chainbell@gmail.com>
2023-01-22 16:36:55 +0900

    init project

---
+++ src/main/java/com/javis/txt/TxtApplication.java
@@ -0,0 +1,13 @@
+package com.javis.txt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TxtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TxtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javis/txt/TxtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javis.txt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TxtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
