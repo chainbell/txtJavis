e4a5ce2bfd223ca43e035e8a97824ef2526009f9
GD-Yoon <chainbell@gmail.com>
2023-01-22 19:25:55 +0900

    thymleaf 설정 추가 & 동작 확인

--- src/main/java/com/javis/txt/app/route/web/TestController.java
+++ src/main/java/com/javis/txt/app/route/web/TestController.java
@@ -1,8 +1,15 @@
 package com.javis.txt.app.route.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class TestController {
 
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("test", "test");
+        return "index";
+    }
 }
