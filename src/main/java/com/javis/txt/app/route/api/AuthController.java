20e4c9b85dd7016b9a737feb346f8814fdcf70d7
GD-Yoon <chainbell@gmail.com>
2023-01-22 19:40:21 +0900

    AuthService Structure 추가

---
+++ src/main/java/com/javis/txt/app/route/api/AuthController.java
@@ -0,0 +1,18 @@
+package com.javis.txt.app.route.api;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthController {
+
+    /**
+     * 로그인 시도
+     * */
+
+    /**
+     * 로그아웃
+     * */
+
+
+
+}
---
+++ src/main/java/com/javis/txt/app/service/impl/AuthServiceImpl.java
@@ -0,0 +1,6 @@
+package com.javis.txt.app.service.impl;
+
+import com.javis.txt.app.service.AuthService;
+
+public class AuthServiceImpl implements AuthService {
+}
