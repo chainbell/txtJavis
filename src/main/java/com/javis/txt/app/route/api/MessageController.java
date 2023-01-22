4b3ff12e8ce4ca7ae67f02aeb62f62bb79616a93
GD-Yoon <chainbell@gmail.com>
2023-01-22 18:47:08 +0900

    init project structure

---
+++ src/main/java/com/javis/txt/app/route/api/MessageController.java
@@ -0,0 +1,7 @@
+package com.javis.txt.app.route.api;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MessageController {
+}
---
+++ src/main/java/com/javis/txt/app/route/web/TestController.java
@@ -0,0 +1,8 @@
+package com.javis.txt.app.route.web;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class TestController {
+
+}
