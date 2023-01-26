a68f8d60f515d3b7693a006a56de1accea246b1f
윤상규 <mylifeis123@smtown.com>
2023-01-26 12:06:18 +0900

    requestmapping, index vue3 & axios 예제 추가

--- src/main/java/com/javis/txt/app/route/api/AuthController.java
+++ src/main/java/com/javis/txt/app/route/api/AuthController.java
@@ -2,12 +2,16 @@ package com.javis.txt.app.route.api;
 
 import com.javis.txt.app.service.AuthService;
 import com.javis.txt.dto.auth.AuthInfoDTO;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/auth")
+@Slf4j
+@RestController
+@RequestMapping(value = "/auth")
 public class AuthController {
 
     @Autowired
@@ -18,7 +22,8 @@ public class AuthController {
      */
     @GetMapping("/admin/login")
     public AuthInfoDTO getLoginInfo(@RequestParam(name = "id") String id, @RequestParam(name = "password") String password) {
-
+        log.info("/auth/admin/login");
+        System.out.println("진입");
         AuthInfoDTO result = authService.getAdminAuthInfo(id, password);
 
         return result;
--- src/test/java/com/javis/txt/TxtApplicationTests.java
+++ src/test/java/com/javis/txt/TxtApplicationTests.java
@@ -12,25 +12,4 @@ class TxtApplicationTests {
 
 	}
 
-//	@Test
-//	void test(){
-//		String url = "jdbc:mariadb://mylifeis123.synology.me:3306/message?useSSL=false&useUnicode=true&serverTimezone=Asia/Seoul";
-//		String username = "developer";
-//		String password = "Roqkfgkqtlek12345!@";
-//
-//		System.out.println(jasyptEncoding(url));
-//		System.out.println(jasyptEncoding(username));
-//		System.out.println(jasyptEncoding(password));
-//	}
-//
-//	public String jasyptEncoding(String value) {
-//
-//		String key = "sunrisdedyet";
-//		StandardPBEStringEncryptor pbeEnc = new StandardPBEStringEncryptor();
-//		pbeEnc.setAlgorithm("PBEWithMD5AndDES");
-//		pbeEnc.setPassword(key);
-//		return pbeEnc.encrypt(value);
-//	}
-
-
 }
