43a34ef1293bef2a506ab837ed108112bfaf0031
윤상규 <mylifeis123@smtown.com>
2023-02-13 17:09:07 +0900

    서비스 함수 설명 주석 추가

--- src/main/java/com/javis/txt/app/service/MessageService.java
+++ src/main/java/com/javis/txt/app/service/MessageService.java
@@ -4,8 +4,10 @@ import java.util.List;
 
 public interface MessageService {
 
+    // 1. 메시지 조회
     List getMessge(String authKey);
 
+    // 2. 메시지 저장(발송)
     boolean setMessage(String authKey, String message);
-    
+
 }
