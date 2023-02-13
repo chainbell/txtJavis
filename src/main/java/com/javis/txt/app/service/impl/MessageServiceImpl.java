f2c7184d692a4224bdf08d7355fabca243fcc9d7
윤상규 <mylifeis123@smtown.com>
2023-02-13 16:50:11 +0900

    예상 작업 주석 정리

--- src/main/java/com/javis/txt/app/service/impl/MessageServiceImpl.java
+++ src/main/java/com/javis/txt/app/service/impl/MessageServiceImpl.java
@@ -12,12 +12,22 @@ public class MessageServiceImpl implements MessageService {
 
     @Override
     public List getMessge(String authKey) {
+
+        // 1. 메시지 최신 기준 조회
+
         return null;
     }
 
     @Override
     public boolean setMessage(String authKey, String message) {
+
+        // 1. message type 추출 - message 포맷 필요 (최초 입력 특수문자를 기준으로 분기 - enum 정리 필요)
+
+        // 2. message type 별 데이터 처리 서비스 로직 호출
+        // 메시지 저장 (메시지 타입, 메시지 데이터 타입, 메시지)
+
         return false;
+
     }
 
 }
